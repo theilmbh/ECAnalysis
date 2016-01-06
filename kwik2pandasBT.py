8378bb7c84f598577a12af4e91003894e46c01d5
brad <btheilma@ucsd.edu>
2016-01-06 08:28:12 -0800

    CLI for kwik2pandas should be in place

--- kwik2pandasBT.py
+++ kwik2pandasBT.py
@@ -22,31 +22,6 @@ class StimCounter(Counter):
         self[key] += 1
         return self[key] - 1
 
-
-
-
-with h5py.File(kwikfile, 'r') as f:
-    sample_rate = None
-    for recording in f['recordings']:
-        assert sample_rate is None or sample_rate == f['recordings'][recording].attrs['sample_rate']
-        sample_rate = f['recordings'][recording].attrs['sample_rate']
-    spikes = pd.DataFrame({ 'time_stamp' : f['channel_groups']['0']['spikes']['time_samples'],
-                            'cluster' : f['channel_groups']['0']['spikes']['clusters']['main'] })
-    cluster_groups = {}
-    for cluster in f['channel_groups']['0']['clusters']['main'].keys():
-        cluster_groups[int(cluster)] = f['channel_groups']['0']['clusters']['main'][cluster].attrs['cluster_group']
-    spikes['cluster_group'] = spikes['cluster'].map(cluster_groups)
-    
-    with f['event_types']['DigMark']['codes'].astype('str'):
-        stims = pd.DataFrame({ 'time_stamp' : f['event_types']['DigMark']['time_samples'],
-                               'code' : f['event_types']['DigMark']['codes'][:] })
-    stims.loc[stims.code == '<', 'stim_end_time_stamp'] = stims[stims['code'] == '>']['time_stamp'].values
-    stims = stims[stims['code'] == '<']
-    stims['stim_name'] = f['event_types']['Stimulus']['text'][1::2]
-stims['stim_presentation'] = stims[stims['code'] == '<']['stim_name'].map(StimCounter().count)
-stims.reset_index(drop=True, inplace=True)
-
-
 class Stims(object):
     # ['code', 'time_stamp', 'stim_end_time_stamp', 'stim_name', 'stim_presentation']
     # TODO: duplicate spikes that are <2 sec from 2 stimuli
@@ -78,11 +53,6 @@ class Stims(object):
             else:
                 return self.cur_stim[[3, 4, 1]]
 
-spikes['stim_name'], spikes['stim_presentation'], spikes['stim_time_stamp'] = zip(*spikes["time_stamp"].map(Stims(stims).stim_checker))
-
-spikes['stim_aligned_time_stamp'] = spikes['time_stamp'].values.astype('int') - spikes['stim_time_stamp'].values
-spikes_wo_bad = spikes[spikes['cluster_group'] != 0]
-
 def compute_refractory_violations(spikes, refrac_T=1.0, sample_rate):
     #Look for refractory period violations
     refrac_samps = (refrac_T / 1000.0) * sample_rate
@@ -94,3 +64,43 @@ def compute_refractory_violations(spikes, refrac_T=1.0, sample_rate):
         violation_prop = 100.0*n_violations / np.size(spike_times_this_cluster)
         refractory_violations.loc[refractory_violations['cluster'] == cluster, 'violations'] = n_violations
         refractory_violations.loc[refractory_violations['cluster'] == cluster, 'percentage'] = violation_prop
+
+
+def main():
+    args = get_args()
+    kwikfile = os.path.abspath(args.kwikfile)
+    dest = os.path.abspath(args.destination_dir)
+
+    kwikfilename = os.path.splitext(os.path.basename(kwikfile))
+    destfilename = kwikfilename
+    destfile = os.path.join(dest, destfilename)
+
+    with h5py.File(kwikfile, 'r') as f:
+        sample_rate = None
+        for recording in f['recordings']:
+            assert sample_rate is None or sample_rate == f['recordings'][recording].attrs['sample_rate']
+            sample_rate = f['recordings'][recording].attrs['sample_rate']
+        spikes = pd.DataFrame({ 'time_stamp' : f['channel_groups']['0']['spikes']['time_samples'],
+                                'cluster' : f['channel_groups']['0']['spikes']['clusters']['main'] })
+        cluster_groups = {}
+        for cluster in f['channel_groups']['0']['clusters']['main'].keys():
+            cluster_groups[int(cluster)] = f['channel_groups']['0']['clusters']['main'][cluster].attrs['cluster_group']
+        spikes['cluster_group'] = spikes['cluster'].map(cluster_groups)
+        
+        with f['event_types']['DigMark']['codes'].astype('str'):
+            stims = pd.DataFrame({ 'time_stamp' : f['event_types']['DigMark']['time_samples'],
+                                   'code' : f['event_types']['DigMark']['codes'][:] })
+        stims.loc[stims.code == '<', 'stim_end_time_stamp'] = stims[stims['code'] == '>']['time_stamp'].values
+        stims = stims[stims['code'] == '<']
+        stims['stim_name'] = f['event_types']['Stimulus']['text'][1::2]
+        stims['stim_presentation'] = stims[stims['code'] == '<']['stim_name'].map(StimCounter().count)
+        stims.reset_index(drop=True, inplace=True)
+        spikes['stim_name'], spikes['stim_presentation'], spikes['stim_time_stamp'] = zip(*spikes["time_stamp"].map(Stims(stims).stim_checker))
+        spikes['stim_aligned_time_stamp'] = spikes['time_stamp'].values.astype('int') - spikes['stim_time_stamp'].values
+        spikes_wo_bad = spikes[spikes['cluster_group'] != 0]
+        spikes_wo_bad.to_pickle(destfile)
+
+
+
+
+
