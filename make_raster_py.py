200497fab062473f6bca0d74fadbe003d10bd65c
brad <btheilma@ucsd.edu>
2016-01-07 08:44:05 -0800

    Added simple make_raster common routine

--- make_raster_py.py
+++ make_raster_py.py
@@ -34,5 +34,20 @@ def make_raster(events, cell, stim, experiment):
 		stim: Dict with StimName, StartTime, EndTime
 		experiment: Dict with Bird, Site XYZ, DataFrame
 	'''
-	
 
+	raster = plt.figure()
+	ntrials = events['trial'].unique().size
+	for i in range(ntrials):
+		spikes_this_trial = events.loc[events['trial'] == i+1, 'TOE'].values.astype('float')]
+		# get number of spikes in this trial
+		nspikes = np.size(spikes_this_trial)
+		# generate ydata for this trial
+		ylimits = [i, i+1]
+
+		for j in range(nspikes):
+			ydata = ylimits
+			xdata = [spikes_this_trial[j], spikes_this_trial[j]]
+			raster.plot(xdata, ydata)
+
+def save_raster(spike_raster, dest):
+	spike_raster.savefig(dest)
