2741b6ed989ea603f23bbb6af54d65e1bee9d5e8
brad <btheilma@ucsd.edu>
2016-01-07 11:44:12 -0800

    make_raster works on test case

--- make_raster_py.py
+++ make_raster_py.py
@@ -13,12 +13,13 @@ import seaborn as sns
 
 def get_args():
     parser = argparse.ArgumentParser(description='Convert manually sorted KWIK file to pandas DataFrame')
-    parser.add_argument('datafile', type=str, nargs='?', help='Path to PANDAS DataFrame containing spike data')
-    parser.add_argument('destination_dir', type=str, nargs='?', help='Directory in which to place raster plots')
+    parser.add_argument('datafile', nargs='?', help='Path to PANDAS DataFrame containing spike data')
+    parser.add_argument('dest', default='./', nargs='?', help='Directory in which to place raster plots')
 
     return parser.parse_args()
 
 def main():
+	print('Make Raster')
 	args = get_args()
 	datafile = os.path.abspath(args.datafile)
 	dest = os.path.abspath(args.dest)
@@ -38,7 +39,7 @@ def make_raster(events, cell, stim, experiment):
 	raster = plt.figure()
 	ntrials = events['trial'].unique().size
 	for i in range(ntrials):
-		spikes_this_trial = events.loc[events['trial'] == i+1, 'TOE'].values.astype('float')]
+		spikes_this_trial = events.loc[events['trial'] == i+1, 'TOE'].values.astype('float')
 		# get number of spikes in this trial
 		nspikes = np.size(spikes_this_trial)
 		# generate ydata for this trial
@@ -47,7 +48,15 @@ def make_raster(events, cell, stim, experiment):
 		for j in range(nspikes):
 			ydata = ylimits
 			xdata = [spikes_this_trial[j], spikes_this_trial[j]]
-			raster.plot(xdata, ydata)
+			plt.plot(xdata, ydata, 'r')
+
+	plt.title("Bird: " + experiment['bird'] + " Cell: " + str(cell['cellid']) + " Type: " + cell['sort_type'] + " Stim: " + stim['stim_name'])
+	plt.xlabel('Time (s)')
+	plt.ylabel('Trial')
+	return raster
 
 def save_raster(spike_raster, dest):
 	spike_raster.savefig(dest)
+
+if __name__ == '__main__':
+    main()
